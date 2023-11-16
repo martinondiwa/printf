cc10dda5815f1cb11b8b2e31f16a7e9b20195664
martinondiwa <surekondiwa@gmail.com>
2023-11-15 20:30:45 -0800

    conversion specifiers

---
+++ conversion.c
@@ -0,0 +1,94 @@
+#include "main.h"
+
+/**
+ * print_i - Prints an integer
+ * @args: Argument to print
+ * Return: Number of digits printed
+ */
+int print_i(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+
+	putchar(last + '0');
+	return (i);
+}
+
+/**
+ * print_d - Prints a decimal
+ * @args: Argument to print
+ * Return: Number of digits printed
+ */
+int print_d(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit;
+	int i = 1;
+	int exp = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+
+	putchar(last + '0');
+	return (i);
+}
