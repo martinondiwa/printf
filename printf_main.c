7f68e93ec5cbd21f82ccf3a1d7f7251d7ecdc70e
martinondiwa <surekondiwa@gmail.com>
2023-11-18 13:15:44 -0800

    more printf

---
+++ printf_main.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	const p[] = {
+		{"%s", print_string},
+		{"%c", print_character},
+		{"%%", print_ % },
+		{"%i", print_iprint_int},
+		{"%d",  print_iprint_int}, {"%r", print_string_reverse},
+		{"%R", print_rot13}, {"%b", print_binary},
+		{"%u", print_unsign},
+		{"%o", print_octal}, {"%x", print_x}, {"%X", print_X},
+		{"%S", print_hex_extra}, {"%p", print_ptr}
+	};
+
+	va_list args;
+	int i = 0, j, length = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
+			{
+				length += p[j].function(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		length++;
+		i++;
+	}
+	va_end(args);
+	return (length);
+}
